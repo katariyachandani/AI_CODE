96bec84a222e0f2127b9c07a70513c6dd4a62ddb
katariyachandani <134923224+katariyachandani@users.noreply.github.com>
2023-05-30 10:58:24 +0530

    Add files via upload

--- job_sheduling.py
+++ job_sheduling.py
@@ -1,28 +1,26 @@
 def schedule_jobs(jobs):
-    sorted_jobs = sorted(jobs, key=lambda x: x[1], reverse=True)
+    sorted_jobs = sorted(jobs, key = lambda x: x[1], reverse = True)
     schedule = []
     current_time = 0
-
+    
     for job in sorted_jobs:
-        job_id, duration = job
+        job_id,profit,duration = job
         start_time = current_time
-        finish_time = current_time + duration
+        finish_time = current_time+duration
         current_time = finish_time
-        schedule.append((job_id, start_time, finish_time))
-
+        schedule.append((job_id, start_time,finish_time))
+        
     return schedule
-
-
-num_jobs = int(input("Enter the number of jobs: "))
+   
+nums_jobs = int(input("enter the no of jobs:"))
 jobs = []
-for i in range(num_jobs):
-    job_id = input(f"Enter the job ID for job {i+1}: ")
-    duration = int(input(f"Enter the duration for job {i+1}: "))
-    jobs.append((job_id, duration))
-
-
+for i in range(nums_jobs):
+    job_id = int(input(f"enter the job id for job {i+1}"))
+    profit = int(input(f"enter the profit for job {i+1}"))
+    duration = int(input(f"enter the duration for job {i+1}"))
+    jobs.append((job_id,profit,duration))
+    
 schedule = schedule_jobs(jobs)
 
-
 for job in schedule:
-    print(f"Job {job[0]}: start time = {job[1]}, finish time = {job[2]}")
+    print(f"job {job[0]} : start time = {job[1]} : finish time = {job[2]}")
